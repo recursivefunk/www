5bceb260361e0c3d66aeffa5141ba2f7d9507dca
recursivefunk <johnny@johnnyray.me>
2018-09-22 20:45:36 -0400

    deploy script changes

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,23 +2,19 @@
 
 const shell = require('shelljs')
 const env = require('good-env')
+const endsWithAny = (suffixes, string) => suffixes.some(s => string.endsWith(s))
+const extentions = ['.js', '.vue', '.scss']
+let sourceChanged
 let cfId
+let files
 
 env.ensure('WEB_CF_ID')
-
 cfId = env.get('WEB_CF_ID')
 
-const endsWithAny = (suffixes, string) => (
-  suffixes.some(s => string.endsWith(s))
-)
-
-const extentions = ['.js', '.vue', '.scss']
-const files = shell.exec('git diff --name-only HEAD~1').split('\n')
-const codeChanged = files.some(f => {
-  if (endsWithAny(extentions, f)) {
-    return true
-  }
-})
+console.log('\n============ CHANGES ============\n')
+files = shell.exec('git diff --name-only HEAD~1').split('\n')
+codeChanged = files.some(f => endsWithAny(extentions, f))
+console.log('\n=================================\n')
 
 if (codeChanged) {
   if (shell.exec('./node_modules/.bin/serverless deploy -v --staging production').code !== 0) {
--- scripts/deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Upload to S3. This could very well just be an S3 sync thing, but I started
-# down a different, serverless-based path and ended up simplifying it
-./node_modules/.bin/serverless deploy -v --staging production
-# Invalidation the cloudfront distro
-aws cloudfront create-invalidation --distribution-id ${WEB_CF_ID} --paths "/*"
