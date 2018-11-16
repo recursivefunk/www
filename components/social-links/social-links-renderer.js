75107ad427b8ecb44a2e00979ea2020805721bd8
recursivefunk <johnny@johnnyray.me>
2018-11-15 21:18:12 -0500

    slight social links renderer refactor

--- components/social-links/social-links-renderer.js
+++ components/social-links/social-links-renderer.js
@@ -1,15 +1,17 @@
 
 export default function (h) {
-  const links = this.links.map(link => {
-    const icon = `icon-${link.name}`
-    return (
+  const links = this.links
+    .map(link => ({
+      icon: `icon-${link.name}`,
+      href: link.href
+    }))
+    .map(link =>
       <li class="Social-Links__item">
         <a href={link.href} target="_blank" class="Social-Links__link">
-          <span class={icon}></span>
+          <span class={link.icon}></span>
         </a>
       </li>
     )
-  })
 
   return (
     <ul class="Social-Links">{links}</ul>
