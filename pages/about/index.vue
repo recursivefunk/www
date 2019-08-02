aa50955622ef7f2ab7846da0d90ec3988e8b513d
recursivefunk <johnny@johnnyray.me>
2019-08-02 09:28:35 -0400

    refactor: add new headshot

--- pages/about/index.vue
+++ pages/about/index.vue
@@ -10,7 +10,7 @@
                 </funky-title>
                 <div class="Media-Text-Grid">
                     <media-text img="/img/kid-me.jpg"></media-text>
-                    <media-text img="/img/me-no-look.jpg" displayWhen="show-lg-up"></media-text>
+                    <media-text img="/img/till-headshot.jpg" displayWhen="show-lg-up"></media-text>
                 </div>
             </section>
             <section class="Section">
