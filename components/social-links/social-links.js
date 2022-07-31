4cbb6506cfe2f01df48996705b3c3a6f74dff334
Johnny <stuff@johnny.email>
2022-07-31 14:28:57 -0400

    chore: adjust footer

--- components/social-links/social-links.js
+++ components/social-links/social-links.js
@@ -11,20 +11,12 @@ export default {
           href: 'https://twitter.com/recursivefunk'
         },
         {
-          name: 'medium',
-          href: 'https://medium.com/@recursivefunk'
-        },
-        {
           name: 'linkedin',
           href: 'https://linkedin.com/in/johnnyrayaustin'
         },
         {
           name: 'github',
           href: 'https://github.com/recursivefunk'
-        },
-        {
-          name: 'instagram',
-          href: 'https://instagram.com/recursivefunk'
         }
       ]
     }
